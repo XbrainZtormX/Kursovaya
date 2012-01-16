8d07d31a5c5d772eedb33c8bd9bf4f216f88dde6
Corpix <me@corpix.ru>
2012-01-16 22:14:37 +0400

    Async

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xml2js = require('xml2js')
   , fs = require('fs')
+  , async = require('async')
   , readFile = function(path){ return fs.readFileSync(__dirname + '/data/' + path).toString(); };
 
 var parser = new xml2js.Parser();
@@ -8,6 +9,8 @@ var parser = new xml2js.Parser();
 var graphml = readFile('graphml.xml')
   , map = readFile('map.osm');
 
-parser.parseString(graphml, function(err, result){
-  console.log(result)
-})
+async.map([ graphml, map ], parser.parseString, function(err, result){
+  if(err)
+    throw new Error(err);
+  console.log(result[1])
+});
